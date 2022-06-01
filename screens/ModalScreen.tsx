f2d053432f2fe76e71cdd1c309253518e9d6240b
ScottyPoi <scott.simpson@ethereum.org>
2022-06-01 15:20:37 -0600

    make mock devtools modal

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,15 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
-
+import { Platform, StyleSheet, TextInput } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
-
+import { Input } from '@rneui/themed';
 export default function ModalScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Dev Tools</Text>
+    <View style={styles.container0}>
+      <Text style={styles.title}>Node Info</Text>
+      <Text style={{textAlign: 'center', fontSize: 20}}>127.0.0.1:5050</Text>
+      <Text selectable style={{textAlign: 'center', fontSize: 20}}>enr:-ISQ4DO6Rma394q7yR5Bvop3U...</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/ModalScreen.tsx" />
+      {/* <EditScreenInfo path="/screens/ModalScreen.tsx" /> */}
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
@@ -19,6 +20,11 @@ export default function ModalScreen() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  container0: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
